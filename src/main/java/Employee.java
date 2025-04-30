2a3e2bc178c1c0e422a3839701cc9b7e7aae8224
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-04-30 10:37:27 -0400

    checkpoint

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,5 +1,18 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
 
-
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String firstName;
+    private String lastName;
+    private String title;
+    private String phoneNumber;
+    private String email;
+    private LocalDate hireDate;
 
 }
